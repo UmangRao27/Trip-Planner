693d5d60ccb2e868d6e086f84d799b0068b235eb
Your Name <you@example.com>
2024-12-22 00:51:11 +0530

    another commit

---
+++ tripplanner/src/Components/Tours.jsx
@@ -0,0 +1,14 @@
+
+
+function Tours() {
+
+    return(
+        <div>
+            <div>
+            <h2>Plan Your Trip</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Tours;
---
+++ tripplanner/src/data.jsx
@@ -0,0 +1,51 @@
+const data = [
+    {
+        id: 1,
+        Name:
+        info:
+        image:
+        price:
+    },
+    {
+        id: 2,
+        Name:
+        info:
+        image:
+        price:
+    },
+    {
+        id: 3,
+        Name:
+        info:
+        image:
+        price:
+    },
+    {
+        id: 4,
+        Name:
+        info:
+        image:
+        price:
+    },
+    {
+        id: 5,
+        Name:
+        info:
+        image:
+        price:
+    },
+    {
+        id: 6,
+        Name:
+        info:
+        image:
+        price:
+    },
+    {
+        id: 7,
+        Name:
+        info:
+        image:
+        price:
+    }
+]
