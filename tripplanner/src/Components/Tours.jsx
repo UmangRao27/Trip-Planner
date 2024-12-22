a12f145864b0c3d76b9a86d448182a3815ed69f8
Your Name <you@example.com>
2024-12-22 23:02:15 +0530

    Card details added

--- tripplanner/src/Components/Tours.jsx
+++ tripplanner/src/Components/Tours.jsx
@@ -1,11 +1,18 @@
 
 
-function Tours() {
+function Tours({Tours}) {
 
     return(
         <div>
             <div>
             <h2>Plan Your Trip</h2>
+            </div>
+            <div>
+                {
+                    Tours.map( (Tour) => {
+                        return <Card {...Tour}></Card>
+                    } )
+                }
             </div>
         </div>
     )
