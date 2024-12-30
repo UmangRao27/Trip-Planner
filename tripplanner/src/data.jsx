96fc1dc1b32b9ffad974258bbaef648b7134e910
Your Name <you@example.com>
2024-12-31 02:35:44 +0530

    Data Added

--- tripplanner/src/data.jsx
+++ tripplanner/src/data.jsx
@@ -4,50 +4,50 @@ const data = [
     {
         id: 1,
         Name:"Agra",
-        info:"",
-        image:"",
+        info:"Agra, located in Uttar Pradesh, India, is famed for housing one of the most iconic landmarks in the world, the Taj Mahal. This majestic white marble mausoleum was built by Emperor Shah Jahan in memory of his wife Mumtaz Mahal. Agra is also home to historical sites such as the Agra Fort and Fatehpur Sikri, both UNESCO World Heritage Sites. The city's rich Mughal heritage and proximity to Delhi make it a popular tourist destination.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Agra.webp",
         price:"34999",
     },
     {
         id: 2,
         Name:"Jaipur",
-        info:"",
-        image:"",
+        info:"Jaipur, the capital of Rajasthan, is known as the 'Pink City' due to its distinct pink-colored architecture. Founded in 1727 by Maharaja Jai Singh II, Jaipur boasts landmarks like the Amber Fort, City Palace, and Hawa Mahal. The city’s blend of history, culture, and art makes it a major attraction for travelers, especially those visiting the famous Golden Triangle of India.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Jaipur.webp",
         price:"29999",
     },
     {
         id: 3,
         Name:"Goa",
-        info:"",
-        image:"",
+        info:"Goa, a coastal state in western India, is renowned for its beautiful beaches, vibrant nightlife, and colonial Portuguese architecture. Its sandy shores, such as Anjuna, Baga, and Calangute, are major attractions for beach lovers. Goa's rich history and mix of Indian and Portuguese cultures are reflected in its churches, forts, and festivals.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Goa.webp",
         price:"41999"
     },
     {
         id: 4,
         Name:"Varanasi",
-        info:"",
-        image:"",
+        info:"Varanasi, one of the oldest living cities in the world, is situated on the banks of the Ganges River in Uttar Pradesh. Known for its spiritual significance in Hinduism, Varanasi is famous for its ghats, where pilgrims perform rituals. The Kashi Vishwanath Temple and the Sarnath ruins are key attractions for history and culture enthusiasts.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Varanasi.webp",
         price:"37999"
     },
     {
         id: 5,
         Name:"Darjeeling",
-        info:"",
-        image:"",
+        info:"Darjeeling, located in West Bengal, is a popular hill station known for its tea plantations, stunning views of the Himalayas, and the iconic Toy Train. The Tiger Hill offers panoramic views of Mount Kanchenjunga, the third-highest mountain in the world. Darjeeling’s cool climate, British-era architecture, and Tibetan culture make it a serene destination.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Darjeeling.webp",
         price:"46999"
     },
     {
         id: 6,
         Name:"Jaisalmer",
-        info:"",
-        image:"",
+        info:"Jaisalmer, also called the 'Golden City' is located in the Rajasthan desert. Known for its yellow sandstone architecture, Jaisalmer’s Jaisalmer Fort is a UNESCO World Heritage Site and one of the largest fully preserved fortified cities in the world. The nearby Sam Sand Dunes offer a unique desert safari experience, and the Patwon Ki Haveli is a fine example of Rajasthani architecture.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Jaisalmer.webp",
         price:"24999"
     },
     {
         id: 7,
         Name:"Kochi",
-        info:"",
-        image:"",
+        info:"Kochi, a major port city in Kerala, is known for its eclectic blend of cultures, including Dutch, Portuguese, and British influences. Fort Kochi is a historical area with landmarks like the Chinese fishing nets and St. Francis Church. Kochi is also famous for its backwaters, the Kerala Kathakali Centre, and its role in the spice trade. The city is a gateway to the lush landscapes of Kerala.",
+        image:"C:\Users\ umang rao\OneDrive\Desktop\vscode file\Web Dev\React JS\Projects React\Trip Planner\tripplanner\src\assets\Kochi.webp",
         price:"32999"
     }
 ]
